e9eccad40bf35d9e1c8d3e91848acefd1dbd5a46
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-16 18:08:53 +0200

    update layout component to display navbar, heading & footer for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { spaceFont } from "@/lib/fonts";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Heading from "@/components/Heading";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={spaceFont}>
+      <Heading />
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
