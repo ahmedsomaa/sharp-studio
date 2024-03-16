4c59b5e67d1c8e1c26c763e9442ec2787274f505
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-16 02:45:40 +0200

    add hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,52 +1,29 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-
-import { spaceFont } from "@/lib/fonts";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import BackgroundGradient from "./BackgoundGradient";
 
 export default function Hero() {
   return (
-    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-14 relative">
-      <div className="flex flex-col md:flex-col md:space-x-4 md:space-y-0 items-center gap-5">
-        <div className="relative mx-auto max-w-3xl text-center gap-3">
-          <h1
-            className={cn(
-              "bg-gradient-to-br from-black to-zinc-600 bg-clip-text text-4xl/[1.07] font-bold tracking-tight text-transparent md:text-6xl/[1.07]",
-              spaceFont
-            )}
-          >
-            High Image Processing with
-            <span className="text-primary"> Sharp!</span>
-          </h1>
-          <p className="mt-6 text-md font-medium text-zinc-600 md:text-lg">
-            Resize, convert and edit your images with precision and ease!
-          </p>
-          <div className="mt-6 flex flex-col items-center justify-center   gap-y-8">
-            <Button
-              asChild
-              size="lg"
-              variant="default"
-              className="font-semibold"
-            >
-              <Link href="">Get Started</Link>
-            </Button>
-          </div>
-        </div>
-        <div className="-m-2 rounded-xl bg-neutral-900/5 dark:bg-neutral-100/10 p-2 ring-1 ring-inset ring-neutral-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-          <Image
-            alt="Hero"
-            height="400"
-            width="600"
-            src="/img/hero-img.png"
-            style={{
-              aspectRatio: "16/9",
-              objectFit: "cover",
-            }}
-            className="rounded-md bg-white shadow-2xl ring-1 ring-sky-900/10"
-          />
-        </div>
+    <div className="relative h-[350px] w-full  px-4 md:h-[605px] md:px-6 lg:px-8 xl:px-10 2xl:px-0">
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://sharp.pixelplumbing.com/"
+          className="mb-5 cursor-pointer rounded-2xl border border-black px-4 py-1 text-sm text-slate-600 transition duration-300 ease-in-out hover:text-slate-700 sm:text-base"
+        >
+          Powered by <span className="font-bold text-primary">Sharp</span>
+        </a>
+        <h1 className="inline-block text-center text-4xl font-medium tracking-tighter text-dark lg:text-7xl">
+          High Image Processing
+        </h1>
+        <p className="mt-8 text-center text-xl font-light tracking-tight lg:text-3xl">
+          Resize, convert and edit your images with precision and ease!
+        </p>
+      </div>
+      {/* background gradient */}
+      <div className="absolute bottom-0 left-0 right-0 top-0 z-[-1] hidden h-full w-full grid-cols-3 md:grid">
+        <BackgroundGradient />
+        <BackgroundGradient />
+        <BackgroundGradient />
       </div>
     </div>
   );
