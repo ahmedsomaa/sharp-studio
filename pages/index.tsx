a42a06a48a2e2263c914fd22a03b932692bf5c3a
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-16 18:10:23 +0200

    display only the hero & features

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import Heading from "@/components/Heading";
-import { spaceFont } from "@/lib/fonts";
+import Features from "@/components/Features";
 
 export default function Landing() {
   return (
-    <div className={spaceFont}>
-      <Heading />
-      <Navbar />
+    <div>
       <Hero />
-      <Footer />
+      <Features />
     </div>
   );
 }
