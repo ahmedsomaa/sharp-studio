8d741584001127d18003d3a53046a59529eedb60
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-17 00:11:42 +0200

    configure toaster globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "@/components/ui/sonner";
 
 import { spaceFont } from "@/lib/fonts";
 import Footer from "@/components/Footer";
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Navbar />
       <Component {...pageProps} />
       <Footer />
+      <Toaster richColors />
     </div>
   );
 }
