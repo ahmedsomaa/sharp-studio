4389e854d8171cd9c0f4c5420de4800a693602eb
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-16 18:12:35 +0200

    start resizer page

---
+++ pages/resizer.tsx
@@ -0,0 +1,7 @@
+export default function Resizer() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h2 className="text-3xl">Coming soon</h2>
+    </div>
+  );
+}
