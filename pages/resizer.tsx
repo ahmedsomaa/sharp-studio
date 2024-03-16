415386397df6ce79b25d6130576cc648387dd414
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-17 00:13:20 +0200

    handle api errors and add margin bottom

--- pages/resizer.tsx
+++ pages/resizer.tsx
@@ -1,4 +1,6 @@
+import { toast } from "sonner";
 import React, { useState } from "react";
+import { ScalingIcon } from "lucide-react";
 
 import {
   Card,
@@ -61,6 +63,7 @@ export default function Resizer() {
   const onReset = () => {
     setResizedImage(null);
     setSelectedImage(null);
+    setDimensions({ height: "", width: "" });
   };
 
   const onResize = async () => {
@@ -75,101 +78,112 @@ export default function Resizer() {
           image: { base64: img, type: selectedImage?.type },
         }),
       });
+      if (req.status === 413) {
+        const err = await req.text();
+        throw new Error(err);
+      }
       const res = await req.json();
       setLoading(false);
-      setResizedImage(res.data);
+      setResizedImage(res.data.img);
     } catch (error) {
       setLoading(false);
-      console.log(error);
+      toast.error("Image Resize Failed");
     }
   };
 
   return (
-    <div className="relative h-[350px] w-full px-4 md:h-[603px] md:px-6 lg:px-8 xl:px-10 2xl:px-0">
-      <div className="flex h-full max-w-screen-md	 mx-auto w-full flex-col items-center gap-5">
-        <h2 className="sm:text-4xl/ text-3xl max-w-[708px] font-bold text-slate-900">
-          Image Resizer
-        </h2>
-        <h3 className="text-xl text-zinc-500">
-          Quickly change the dimensions of your images.
-        </h3>
-        <Card className="w-full flex flex-col gap-2 shadow-none">
-          <CardHeader>
-            <CardTitle className="text-lg font-medium">
-              Upload your image
-            </CardTitle>
-            <CardDescription className="text-sm font-medium text-gray-500">
-              Only PNG, JPG & WEBP are supported.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid sm:grid-cols-2 lg:grid-cols-2 gap-4">
-            <Dropzone
-              onDrop={handleDrop}
-              onDelete={handleOnDelete}
-              onDragOver={handleDragOver}
-              onChange={handleFileChange}
-              uploadedImage={selectedImage}
-            />
-            <div className="flex flex-col justify-start gap-3">
+    <div className="mb-40">
+      <section className="relative h-[350px] w-full px-4 md:h-[605px] md:px-6 lg:px-8 xl:px-10 2xl:px-0">
+        <div className="flex h-full max-w-screen-md	 mx-auto w-full flex-col items-center gap-5">
+          <h2 className="sm:text-4xl/ text-3xl max-w-[708px] font-bold text-slate-900">
+            <p className="flex flex-row items-center">
+              <ScalingIcon size={32} className="mr-2" />
+              Image Resizer
+            </p>
+          </h2>
+          <h3 className="text-xl text-zinc-500">
+            Quickly change the dimensions of your images.
+          </h3>
+          <Card className="w-full flex flex-col gap-2 shadow-none">
+            <CardHeader>
+              <CardTitle className="text-lg font-medium">
+                Upload your image
+              </CardTitle>
+              <CardDescription className="text-sm font-medium text-gray-500">
+                Only PNG, JPG & WEBP are supported.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid sm:grid-cols-2 lg:grid-cols-2 gap-4">
               <div>
-                <Label htmlFor="height">New Height</Label>
-                <Input
-                  id="height"
-                  type="text"
-                  name="height"
-                  inputMode="numeric"
-                  disabled={!selectedImage}
-                  value={dimensions.height}
-                  placeholder="Enter new height..."
-                  onChange={({ target }) =>
-                    setDimensions((prev) => ({
-                      ...prev,
-                      height: target.value,
-                    }))
-                  }
+                <Dropzone
+                  onDrop={handleDrop}
+                  onDelete={handleOnDelete}
+                  onDragOver={handleDragOver}
+                  onChange={handleFileChange}
+                  uploadedImage={selectedImage}
                 />
               </div>
-              <div>
-                <Label htmlFor="height">New Width</Label>
-                <Input
-                  id="width"
-                  type="text"
-                  name="width"
-                  inputMode="numeric"
-                  value={dimensions.width}
-                  disabled={!selectedImage}
-                  placeholder="Enter new width..."
-                  onChange={({ target }) =>
-                    setDimensions((prev) => ({
-                      ...prev,
-                      width: target.value,
-                    }))
-                  }
-                />
-              </div>
-            </div>
-          </CardContent>
-          {selectedImage && <Separator orientation="horizontal" />}
-          {selectedImage && (
-            <CardFooter className="flex flex-row justify-end">
-              {resizedImage ? (
-                <div className="space-x-2">
-                  <Button variant="outline" onClick={onReset}>
-                    Reset
-                  </Button>
-                  <a href={resizedImage} download>
-                    <Button>Download</Button>
-                  </a>
+              <div className="flex flex-col justify-start gap-3">
+                <div>
+                  <Label htmlFor="height">New Height</Label>
+                  <Input
+                    id="height"
+                    type="text"
+                    name="height"
+                    inputMode="numeric"
+                    disabled={!selectedImage}
+                    value={dimensions.height}
+                    placeholder="Enter new height..."
+                    onChange={({ target }) =>
+                      setDimensions((prev) => ({
+                        ...prev,
+                        height: target.value,
+                      }))
+                    }
+                  />
                 </div>
-              ) : (
-                <Button disabled={loading} onClick={onResize}>
-                  {loading ? "Resizing..." : "Resize"}
-                </Button>
-              )}
-            </CardFooter>
-          )}
-        </Card>
-      </div>
+                <div>
+                  <Label htmlFor="height">New Width</Label>
+                  <Input
+                    id="width"
+                    type="text"
+                    name="width"
+                    inputMode="numeric"
+                    value={dimensions.width}
+                    disabled={!selectedImage}
+                    placeholder="Enter new width..."
+                    onChange={({ target }) =>
+                      setDimensions((prev) => ({
+                        ...prev,
+                        width: target.value,
+                      }))
+                    }
+                  />
+                </div>
+              </div>
+            </CardContent>
+            {selectedImage && <Separator orientation="horizontal" />}
+            {selectedImage && (
+              <CardFooter className="flex flex-row justify-end">
+                {resizedImage ? (
+                  <div className="space-x-2">
+                    <Button variant="outline" onClick={onReset}>
+                      Reset
+                    </Button>
+                    <a href={resizedImage} download>
+                      <Button>Download</Button>
+                    </a>
+                  </div>
+                ) : (
+                  <Button disabled={loading} onClick={onResize}>
+                    {loading ? "Resizing..." : "Resize"}
+                  </Button>
+                )}
+              </CardFooter>
+            )}
+          </Card>
+        </div>
+      </section>
     </div>
   );
 }
