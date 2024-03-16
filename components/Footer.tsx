fa2a6b8eac022587b8e2d8d3b00f790d1f166290
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-16 02:45:18 +0200

    add footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,51 +1,26 @@
-"use client";
-
-import React from "react";
-
-import { cn } from "@/lib/utils";
 import Github from "./icons/Github";
 import Twitter from "./icons/Twitter";
 
-interface FooterProps {
-  bgColor?: string;
-  iconColor?: string;
-}
-
-const Footer = ({ bgColor, iconColor }: FooterProps) => {
+export default function Footer() {
   return (
-    <footer
-      className={cn(
-        "p-4 bg-white border-t border-slate-100  flex items-center justify-center",
-        bgColor
-      )}
-    >
-      <div className="max-w-4xl w-full flex">
-        <div className="flex flex-row justify-between items-center w-full">
-          <p className={cn("text-sm leading-5 text-white", iconColor)}>
-            © Copyright {new Date().getFullYear()} PicLoom.
-          </p>
-          <div className="flex flex-row space-x-4 items-center">
-            <a href="https://github.com/ahmedsomaa" target="_blank">
-              <Github
-                className={cn(
-                  "text-slate-100 hover:text-slate-100/90",
-                  iconColor
-                )}
-              />
-            </a>
-            <a href="https://twitter.com/abokahfa" target="_blank">
-              <Twitter
-                className={cn(
-                  "text-slate-100 hover:text-slate-100/90",
-                  iconColor
-                )}
-              />
-            </a>
-          </div>
-        </div>
+    <footer className="container mx-auto my-5 flex h-16 flex-col items-center justify-between space-y-3 border-t px-3 pt-4 text-center sm:h-20 sm:flex-row sm:pt-2 md:text-lg">
+      <div>© {new Date().getFullYear()} PicLoom</div>
+      <div className="flex space-x-4 pb-4 sm:pb-0">
+        <a
+          href="https://twitter.com/abokahfa"
+          className="group"
+          aria-label="Twitter"
+        >
+          <Twitter />
+        </a>
+        <a
+          href="https://github.com/ahmedsomaa"
+          className="group"
+          aria-label="GitHub"
+        >
+          <Github />
+        </a>
       </div>
     </footer>
   );
-};
-
-export default Footer;
+}
