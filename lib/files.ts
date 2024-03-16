6ae49a91e582b9127ed9ca9fd0913488f6a14459
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-16 22:29:52 +0200

    add toBase64 util method in `lib/files.ts`

---
+++ lib/files.ts
@@ -0,0 +1,15 @@
+export function toBase64(file: File) {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+
+    fileReader.readAsDataURL(file);
+
+    fileReader.onload = () => {
+      resolve(fileReader.result as string);
+    };
+
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
