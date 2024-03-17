f048ef05a2c1193bc94995383d9c85d52958714f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-17 14:46:01 +0200

    rename to HeroGradient

--- components/HeroGradient.tsx
+++ components/HeroGradient.tsx
@@ -1,9 +1,9 @@
-export default function BackgroundGradient() {
+export default function HeroGradient() {
   return (
     <div
       className="h-full w-full rounded-full"
       style={{
-        opacity: "0.4",
+        opacity: "0.5",
         background:
           "radial-gradient(54.14% 54.14% at 50% 50%, #99CC00 0%, rgba(103, 2, 139, 0.02) 100%)",
         filter: "blur(177px)",
