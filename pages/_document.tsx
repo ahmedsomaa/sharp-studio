5be7f601bbfa43b3461b15203eeb8c8348f75710
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-17 16:17:30 +0200

    add vercel speed insights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -7,6 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   );
