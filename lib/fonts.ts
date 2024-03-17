364a792997e9693ec4ccae7eba42d53c117147e6
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-17 02:14:00 +0200

    remove exported inter font instance

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,14 +1,8 @@
-import { Inter, Space_Grotesk } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 
-const paragraph = Inter({
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   display: "swap",
 });
 
-const heading = Space_Grotesk({
-  subsets: ["latin"],
-  display: "swap",
-});
-
-export const spaceFont = heading.className;
-export const interFont = paragraph.className;
+export const spaceFont = spaceGrotesk.className;
