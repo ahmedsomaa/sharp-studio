eb22441a37336987ce5c3864d926f3e429b69089
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-17 04:21:33 +0200

    update app logo and favicon

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,9 @@
-import { PenToolIcon } from "lucide-react";
+import { HashIcon } from "lucide-react";
 
 export default function Logo() {
   return (
     <div className="flex flex-row items-center justify-center gap-2">
-      <PenToolIcon size={30} className="text-primary" />
+      <HashIcon size={30} className="text-primary" />
       <h1 className="text-xl font-medium text-primary md:text-3xl">
         SharpStudio
       </h1>
