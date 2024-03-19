5a8cddf9e16fc784a841215de646255bbff27439
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-20 01:27:35 +0200

    move features to a static array

--- components/sections/Features.tsx
+++ components/sections/Features.tsx
@@ -2,67 +2,57 @@ import Link from "next/link";
 import { RepeatIcon, ScalingIcon, ShrinkIcon } from "lucide-react";
 
 import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import React from "react";
+
+const features = [
+  {
+    title: "Convert",
+    icon: <RepeatIcon size={24} className="group-hover:text-primary" />,
+    path: "/convert",
+    description: "Quickly convert your images from one format to another.",
+  },
+  {
+    title: "Resize",
+    icon: <ScalingIcon size={24} className="group-hover:text-primary" />,
+    path: "/resize",
+    description: "Quickly change the dimensions of your images.",
+  },
+  {
+    title: "Compress",
+    icon: <ShrinkIcon size={24} className="group-hover:text-primary" />,
+    path: "/compress",
+    description: "Quickly compress your images for faster websites.",
+  },
+];
 
 export default function Features() {
   return (
-    <div className="mt-16 mb-44 md:mb-20">
+    <div className="mt-16 mb-80 md:mb-44">
       <section className="relative h-[350px] w-full  px-4 md:h-[350px] md:px-6 lg:px-8 xl:px-10 2xl:px-0">
         <div className="flex h-full w-full flex-col items-center">
           <h2 className="sm:text-5xl/ text-4xl max-w-[708px] font-bold text-slate-900">
             Tools
           </h2>
           <div className="grid sm:grid-cols-2 lg:grid-cols-2 gap-4 mt-14">
-            <Link href="/convert">
-              <Card className="w-[400px] hover:border-primary hover:cursor-pointer">
-                <CardHeader>
-                  <CardTitle>
-                    <div className="flex flex-row items-center gap-2">
-                      <RepeatIcon size={24} />
-                      <h3 className="text-lg font-medium leading-none">
-                        Convert
-                      </h3>
-                    </div>
-                  </CardTitle>
-                  <CardDescription className="line-clamp-2 text-md leading-snug text-muted-foreground">
-                    Quickly convert your images from one format to another.
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-            </Link>
-            <Link href="/resize">
-              <Card className="w-[400px] hover:border-primary hover:cursor-pointer">
-                <CardHeader>
-                  <CardTitle>
-                    <div className="flex flex-row items-center gap-2">
-                      <ScalingIcon size={24} />
-                      <h3 className="text-lg font-medium leading-none">
-                        Resize
-                      </h3>
-                    </div>
-                  </CardTitle>
-                  <CardDescription className="line-clamp-2 text-md leading-snug text-muted-foreground">
-                    Quickly change the dimensions of your images.
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-            </Link>
-            <Link href="/compress">
-              <Card className="w-[400px] hover:border-primary hover:cursor-pointer">
-                <CardHeader>
-                  <CardTitle>
-                    <div className="flex flex-row items-center gap-2">
-                      <ShrinkIcon size={24} />
-                      <h3 className="text-lg font-medium leading-none">
-                        Compress
-                      </h3>
-                    </div>
-                  </CardTitle>
-                  <CardDescription className="line-clamp-2 text-md leading-snug text-muted-foreground">
-                    Quickly compress your images for faster websites.
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-            </Link>
+            {features.map(({ title, icon, path, description }) => (
+              <Link key={title} href={path}>
+                <Card className="w-[400px] group hover:border-primary hover:cursor-pointer">
+                  <CardHeader>
+                    <CardTitle>
+                      <div className="flex flex-col gap-3">
+                        {icon}
+                        <h3 className="text-lg group-hover:text-primary font-medium leading-none">
+                          {title}
+                        </h3>
+                      </div>
+                    </CardTitle>
+                    <CardDescription className="line-clamp-2 text-md leading-snug text-muted-foreground">
+                      {description}
+                    </CardDescription>
+                  </CardHeader>
+                </Card>
+              </Link>
+            ))}
           </div>
         </div>
       </section>
